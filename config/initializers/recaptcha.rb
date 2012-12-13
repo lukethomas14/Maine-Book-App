fc9da7a8c15f19cf1d7b0b52076ebc405944d95d
Luke Thomas <me@lukethomas.com>
2012-12-12 23:50:11 -0500

    new stuff

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LeGTtoSAAAAAHawdIIv2lBCQd923aqUhCkjB2yl'
+  config.private_key = '6LeGTtoSAAAAAGFT7jJPvizHUMQvdl9qwWnaCMfh'
+  config.proxy = 'http://youmainebooks.com'
+end
